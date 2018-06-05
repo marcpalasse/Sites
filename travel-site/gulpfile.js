fe6520943b6df758b8f406170d831bbefd87dc56
Marc <marcpalasse@hotmail.com>
2018-06-05 16:25:32 +0200

    gulp exercises

--- travel-site/gulpfile.js
+++ travel-site/gulpfile.js
@@ -8,8 +8,17 @@ gulp.task('default',function(){
 gulp.task('html', function(){
   console.log("Imagine something useful done to your HTML");
 });
+
+gulp.task('styles', function(){
+  console.log("Imagine Sass or PostCSS tasks running here");
+});
+
 gulp.task('watch', function(){
   watch('./app/index.html', function(){
     gulp.start('html');
-  });
+	});
+  watch('./app/assets/styles/**/*.css', function(){
+	   gulp.start('styles');
+   });
+
 });
