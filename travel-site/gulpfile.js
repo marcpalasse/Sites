dd4755f94e4cef48ab9cd039b584d5bb4841e214
Marc <marcpalasse@hotmail.com>
2018-06-05 13:54:01 +0200

    commit ,gitignore et gulpfile

---
+++ travel-site/gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+
+gulp.task('default',function(){
+  console.log("Hooray - you created a Gulp task.");
+});
+
+gulp.task('html', function(){
+  console.log("Imagine something useful done to your HTML");
+});
+gulp.task('watch', function(){
+  watch('./app/index.html', function(){
+    gulp.start('html');
+  });
+});
